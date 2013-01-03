2ec2f1bdc954b3543422b54ffe97dfc59e8549ed
Chris Violette <cvioletterun@gmail.com>
2013-01-03 09:36:26 -0500

    Updated image sizes

--- wp-content/themes/pchcresidency/functions.php
+++ wp-content/themes/pchcresidency/functions.php
@@ -49,8 +49,9 @@ require_once('library/custom-post-slide.php');
 /************* THUMBNAIL SIZE OPTIONS *************/
 
 // Thumbnail sizes
-add_image_size( 'pchc-thumb-600', 600, 150, true );
-add_image_size( 'pchc-thumb-300', 300, 100, true );
+//add_image_size( 'pchc-thumb-600', 600, 150, true );
+//add_image_size( 'pchc-thumb-300', 300, 100, true );
+add_image_size( 'pchc-featurette', 512, 512, true );
 /* 
 to add more sizes, simply copy a line from above 
 and change the dimensions & name. As long as you
