ffcdc32990073147ef49fc8d697a5230d7e281eb
Chris Violette <cvioletterun@gmail.com>
2013-01-07 16:50:22 -0500

    Styling the footer.

--- wp-content/themes/pchcresidency/footer.php
+++ wp-content/themes/pchcresidency/footer.php
@@ -7,7 +7,7 @@
     					<?php bones_footer_links(); ?>
 	                </nav>
 	                		
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+					<p class="source-org copyright muted">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
 				
 				</div> <!-- end #inner-footer -->
 				
