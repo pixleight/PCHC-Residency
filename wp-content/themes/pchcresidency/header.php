aefaee550e1127f58a399dc1f0a1ec4092bb76ce
Chris Violette <cvioletterun@gmail.com>
2013-01-10 10:47:34 -0500

    Removed meta description tag so the SEO plugin can take care of that

--- wp-content/themes/pchcresidency/header.php
+++ wp-content/themes/pchcresidency/header.php
@@ -18,7 +18,6 @@
 		<meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-		<meta name="description" content="">
 		<meta name="author" content="Chris Violette">
 		
 		<!-- icons & favicons (for more: http://themble.com/support/adding-icons-favicons/) -->
