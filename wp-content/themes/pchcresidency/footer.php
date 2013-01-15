3da04c2e2afe75fb3cf97ca1e8f1d1458672a908
Chris Violette <cvioletterun@gmail.com>
2013-01-15 16:12:02 -0500

    Added browser check script; moved analytics to bottom.

--- wp-content/themes/pchcresidency/footer.php
+++ wp-content/themes/pchcresidency/footer.php
@@ -18,6 +18,32 @@
 		<!-- all js scripts are loaded in library/bones.php -->
 		<?php wp_footer(); ?>
 
+		<!-- drop Google Analytics Here -->
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-37581302-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
+		<!-- end analytics -->
+
+		<!-- Browser check script -->
+		<script>
+		  (function(d, t) {
+		    var e = d.createElement(t);
+		    e.src = d.location.protocol + '//www.browserawarenessday.com/widget/50f5c5369c1b7ef2630000c8';
+		    e.async = true;
+		    var n = d.getElementsByTagName(t)[0]; n.parentNode.insertBefore(e, n);
+		  })(document, 'script');  
+		</script>
+
 	</body>
 
 </html> <!-- end page. what a ride! -->
--- wp-content/themes/pchcresidency/header.php
+++ wp-content/themes/pchcresidency/header.php
@@ -33,22 +33,6 @@
 		<!--[if lt IE 9]>
 			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
-
-		<!-- drop Google Analytics Here -->
-		<script type="text/javascript">
-
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-37581302-1']);
-		  _gaq.push(['_trackPageview']);
-
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-
-		</script>
-		<!-- end analytics -->
 		
 	</head>
 	
